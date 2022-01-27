4d7a2bb98c14a73607280486837360a6ff6056d9
TinySnow <lo.tinysnow.ol@outlook.com>
2022-01-28 00:55:49 +0800

    Updated to leetcode question 2

--- src/Run.java
+++ src/Run.java
@@ -1,11 +1,16 @@
 import leetcode.Question1;
+import necessary.ListNode;
 
 import java.util.Arrays;
 
 public class Run {
     public static void main(String[] args) {
-        int[] test = new int[]{3, 2, 4};
-        int target = 6;
-        System.out.println(Arrays.toString(Question1.twoSum(test, target)));
+//        int[] test = new int[]{3, 2, 4};
+//        int target = 6;
+//        System.out.println(Arrays.toString(Question1.twoSum(test, target)));
+
+//        int a = 9;
+//        int b = 1999999999;
+//        System.out.println(String.valueOf(a + b));
     }
 }
