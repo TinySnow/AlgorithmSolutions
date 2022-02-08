f4b8267130e51fb294e678d202ed03bf8f8c3fc8
TinySnow <lo.tinysnow.ol@outlook.com>
2022-02-08 12:56:27 +0800

    Leetcode question 1001 UNfinished

---
+++ src/leetcode/Question1001.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * UNFINISHED
+ */
+public class Question1001 {
+    public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
+        return null;
+    }
+}
